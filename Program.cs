7f2b44f58ef5a2f9a477de44830cfa615cb76e5f
Melike VURUCU <melikev63@gmail.com>
2019-12-11 17:03:42 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace myCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number: ");
+            double first = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter another number: ");
+            double second = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter an operation:");
+            Console.Write("+ -> addition, - -> subtraction, * -> multiplication, / division ");
+            char opt = Convert.ToChar(Console.ReadLine());
+            switch (opt){
+                case '+':
+                    Console.WriteLine("{0} + {1} = {2}", first, second, (first + second));
+                    break;
+                case '-':
+                    Console.WriteLine("{0} - {1} = {2}", first, second, (first - second));
+                    break;
+                case '*':
+                    Console.WriteLine("{0} * {1} = {2}", first, second, (first * second));
+                    break;
+                case '/':
+                    if (second == 0){
+                        Console.WriteLine("Error: You're trying to divide by 0.");
+                    }
+                    else{
+                        Console.WriteLine("{0} / {1} = {2}", first, second, (first / second));
+                    }
+                    break;
+                default:
+                    Console.WriteLine("You may have chosen wrong operation.");
+                    break;
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
